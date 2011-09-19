e34536805748e492e8f45974ef5fd623741b534b
anthrax11 <andres.traks@gmail.com>
2011-09-19 17:40:38 +0300

    Partial graph panning.

--- test/container/source/class/container/Graph.js
+++ test/container/source/class/container/Graph.js
@@ -29,12 +29,18 @@ qx.Class.define("container.Graph",
 
     this.__grapharea = this._createChildControl("grapharea");
     this.__grapharea.setCursor("move");
+    
+    this.addListener("mousedown", this._onMouseDown);
+    this.addListener("mouseup", this._onMouseUp);
+    this.addListener("mousemove", this._onMouseMove);
   },
 
   members :
   {
     __grapharea : null,
     __posleft : 0,
+    __ispanning : false,
+    __pananchor : {left : null, top : null},
   
     _createChildControlImpl : function(id, hash)
     {
@@ -49,6 +55,53 @@ qx.Class.define("container.Graph",
       }
 
       return control || this.base(arguments, id);
+    },
+    
+    _onMouseDown : function(e)
+    {
+      if (!e.isLeftPressed()) {
+        return;
+      }
+
+      e.stopPropagation();
+
+      this.__ispanning = true;
+      this.__pananchor = {
+        left : e._native.clientX - this.__layoutProperties.left,
+        top : e._native.clientY - this.__layoutProperties.top}
+      this.__grapharea.setCursor("help");
+    },
+    
+    _onMouseUp : function(e)
+    {
+      if (!e.isLeftPressed()) {
+        return;
+      }
+
+      e.stopPropagation();
+
+      this.__ispanning = false;
+      this.__grapharea.setCursor("move");
+    },
+    
+    _onMouseMove : function(e)
+    {
+      if (!e.isLeftPressed()) {
+        return;
+      }
+
+      if (this.__ispanning)
+      {
+        e.stopPropagation();
+        
+        var currentpos = {
+        left : e._native.clientX - this.__layoutProperties.left,
+        top : e._native.clientY - this.__layoutProperties.top}
+        
+        //this.scrollToX(this.getScrollX() - (this.__pananchor.left - currentpos.left));
+        this.scrollToX((this.__pananchor.left - currentpos.left));
+        this.__grapharea.setCursor("help");
+      }
     },
     
     addItem : function(widget)
