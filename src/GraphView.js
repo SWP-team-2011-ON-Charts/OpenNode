7a1d9f9a2f3191fb0369d240f1a6516cd74fdb6d
FableBlaze <oolop7@hotmail.com>
2011-11-12 17:44:19 +0200

    licence info

--- src/GraphView.js
+++ src/GraphView.js
@@ -1,3 +1,12 @@
+/*
+
+Copyright (c) 2011 OpenNode Interactive Charts team
+
+GNU General Public License Usage
+This file may be used under the terms of the GNU General Public License version 3.0 as published by the Free Software Foundation and appearing in the file LICENSE included in the packaging of this file.  Please review the following information to ensure the GNU General Public License version 3.0 requirements will be met: http://www.gnu.org/copyleft/gpl.html.
+
+*/
+
 Ext.define('Funcman.GraphView', {
     alias: 'GraphView',
     extend: 'Ext.Container',
